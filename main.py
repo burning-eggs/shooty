7f8e0d90ded182ad5067ec36c83995f470f43fef
wake-code <87005812+wake-code@users.noreply.github.com>
2021-10-01 09:56:49 +0300

    Detect player input (#0001)

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from core.soldier import Soldier
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Shooty")
+moving_left = False
+moving_right = False
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 player = Soldier(200, 200, 3)
+pygame.display.set_caption("Shooty")
 
 run = True
 
@@ -18,6 +20,20 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+
+            if event.key == pygame.K_d:
+                moving_right = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+
+            if event.key == pygame.K_d:
+                moving_right = False
 
     pygame.display.update()
 
